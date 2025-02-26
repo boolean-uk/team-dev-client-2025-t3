3bf176f46e2aa26b52df36c18f04def4985ebb43
stp <steventphung6@gmail.com>
2025-02-26 13:20:25 +0100

    navigation bar, and changed routing

---
+++ src/assets/icons/noteIcon.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+const NoteIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 20"
+      width="45"
+      height="50"
+      fill="currentColor"
+    >
+      <path d="M3 3h18v18H3V3zm2 2v14h14V5H5zm2 3h10v2H7V8zm0 4h10v2H7v-2zm0 4h6v2H7v-2z" />
+    </svg>
+  );
+};
+
+export default NoteIcon;
---
+++ src/components/cohortTeacher/index.js
@@ -0,0 +1,7 @@
+export default function CohortTeacher() {
+  return (
+    <div>
+      <h1>Cohort Teacher</h1>
+    </div>
+  );
+}
